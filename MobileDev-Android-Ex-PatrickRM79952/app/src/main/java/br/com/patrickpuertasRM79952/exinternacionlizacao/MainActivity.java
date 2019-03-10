23d0c256e9aad16eec1d49ecaf85e8888347abe3
Patrick Puertas <patrick.puertas1@gmail.com>
2019-03-10 02:45:52 -0300

    Add files via upload

---
+++ MobileDev-Android-Ex-PatrickRM79952/app/src/main/java/br/com/patrickpuertasRM79952/exinternacionlizacao/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.patrickpuertasRM79952.exinternacionlizacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
